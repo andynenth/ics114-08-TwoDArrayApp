e4a3e3af00bed9dbab0c393ce58eabe1ede347db
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-15 12:04:07 -0800

    in lab

--- TwoDArrayApp/Program.cs
+++ TwoDArrayApp/Program.cs
@@ -6,7 +6,26 @@ namespace TwoDArrayApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[,] twoDArray = new int[100,2];
+
+
+            for (int i = 0; i < twoDArray.GetLongLength(0); i++)
+            {
+                    Random r = new Random();
+                    twoDArray[i, 0] = r.Next(0, 100);
+                    twoDArray[i, 1] = (int)Math.Pow(twoDArray[i, 0], 2);
+            }
+            
+            Console.WriteLine("Random Values Squares\n");
+            Console.WriteLine(String.Format("{0,6} {1,13}" , "Random", "Number"));
+            Console.WriteLine(String.Format("{0,6} {1,13}", "Number", "Squared"));
+            Console.WriteLine("----------------------");
+            for (int i = 0; i < twoDArray.GetLongLength(0); i++)
+            {
+
+                Console.WriteLine(String.Format("{0,5} {1,14}", twoDArray[i, 0], twoDArray[i, 1]));
+            }
         }
     }
 }
